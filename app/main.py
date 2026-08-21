f2c2a1c0dc6fdf5a684cd3b68350bf4a81694ad5
Gianfranco <25282809+ciaffo@users.noreply.github.com>
2026-08-21 16:12:36 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,15 +1,8 @@
-import socket  # noqa: F401
+import socket
+import threading
 
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment the code below to pass the first stage
-    
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, _addr = server_socket.accept()  # wait for client
-
+def handle_connection(conn):
     with conn:
         while True:
             data = conn.recv(1024)
@@ -17,6 +10,17 @@ def main():
                 break
             # Hardcoded RESP response: +PONG\r\n (simple string "PONG")
             conn.sendall(b"+PONG\r\n")
+
+
+def main():
+    # You can use print statements as follows for debugging, they'll be visible when running tests.
+    print("Logs from your program will appear here!")
+
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+
+    while True:
+        conn, _addr = server_socket.accept()  # wait for client
+        threading.Thread(target=handle_connection, args=(conn,), daemon=True).start()
 
 
 if __name__ == "__main__":
